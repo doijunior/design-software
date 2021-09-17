78ef963edfcda73afccb4134cdfd11fe39192fad
Gilson Doi Junior <doijunior@yahoo.com.br>
2021-09-17 19:39:04 -0300

    BTC adapter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import * as readline from 'readline-sync'
-import StateMachine from './StateMachine'
-import Menu from './views/Menu'
-import Add from './views/Add'
-;(async function () {
-  const stateControl: StateMachine = new StateMachine(new Menu())
-  stateControl.addState(new Add())
+import axios from 'axios'
+interface Quote {
+  date: Date,
+  open: number,
+  high: number,	
+  low: number,
+  close: number,
+  adjusted: number,
+  volume: number
+}
 
-  let input = ''
-  do {
-    stateControl.exec('')
-  } while (stateControl.getCurState() !== 'END')
+
+const fromString = ( item: string ): Quote => {
+  const rawArray: string[] = item.split(',')
+  return {
+    date: new Date( rawArray[0] ),
+    open: Number( rawArray[1] ),
+    high: Number( rawArray[2] ),
+    low: Number( rawArray[3] ),
+    close: Number( rawArray[4] ),
+    adjusted: Number( rawArray[5] ),
+    volume: Number( rawArray[6] )
+  }
+}
+
+( async() => {
+  const { data } = await axios.get('https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1600309522&period2=1631845522&interval=1d&events=history&includeAdjustedClose=true')
+  const dataArray = data.split('\n')
+  const itemsArray = dataArray.map((item: string) => {
+    return fromString(item)
+  })
+
+  console.log(itemsArray)
+
 })()

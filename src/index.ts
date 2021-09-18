bb3f9adaa1fac59e38370b5faef561779c0ff1d0
Gilson Doi Junior <doijunior@yahoo.com.br>
2021-09-17 22:26:00 -0300

    version replacing map by for

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
 import axios from 'axios'
+
 interface Quote {
   date: Date,
-  open: number,
-  high: number,	
-  low: number,
-  close: number,
-  adjusted: number,
+  openValue: number,
+  higherValue: number,	
+  lowestValue: number,
+  closeValue: number,
+  adjustedValue: number,
   volume: number
 }
-
 
 const fromString = ( item: string ): Quote => {
   const rawArray: string[] = item.split(',')
   return {
     date: new Date( rawArray[0] ),
-    open: Number( rawArray[1] ),
-    high: Number( rawArray[2] ),
-    low: Number( rawArray[3] ),
-    close: Number( rawArray[4] ),
-    adjusted: Number( rawArray[5] ),
+    openValue: Number( rawArray[1] ),
+    higherValue: Number( rawArray[2] ),
+    lowestValue: Number( rawArray[3] ),
+    closeValue: Number( rawArray[4] ),
+    adjustedValue: Number( rawArray[5] ),
     volume: Number( rawArray[6] )
   }
 }
 
-( async() => {
-  const { data } = await axios.get('https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1600309522&period2=1631845522&interval=1d&events=history&includeAdjustedClose=true')
+
+(async function() {
+  const { data } = await axios.get('https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1600387677&period2=1631923677&interval=1d&events=history&includeAdjustedClose=true')
   const dataArray = data.split('\n')
-  const itemsArray = dataArray.map((item: string) => {
-    return fromString(item)
-  })
-
+  const itemsArray: Quote[] = []
+  for (let i = 1, length = dataArray.length; i < length; i++ ) {
+    const item: Quote = fromString( dataArray[i] )
+    itemsArray.push(item)
+  }
   console.log(itemsArray)
-
+  return 0
 })()
+
+
+
